28a9cedb474a28d7a9b58fcc374b810c12ced288
gregm316 <gregm316@gmail.com>
2022-07-28 12:13:49 -0400

    updated set and rep counter in daily routine

--- client/src/pages/DailyRoutine.js
+++ client/src/pages/DailyRoutine.js
@@ -109,7 +109,9 @@ const DailyRoutine = () => {
           {state.cart.map((item) => (
             <CartItem key={item._id} item={item} />
           ))}
+
           <div className="flex-row space-between">
+
             {Auth.loggedIn() ? (
               <button onClick={submitCheckout}>Checkout</button>
             ) : (
