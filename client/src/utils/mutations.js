7a0c5e3a802bb5cff04867901f9bfa14c533cec6
gregm316 <gregm316@gmail.com>
2022-07-28 12:19:06 -0400

    Added query for all exercises

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,52 +1,72 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
+export const QUERY_EXERCISE = gql`
+  query getExercises($category: ID) {
+    exercises(category: $category) {
+      _id
+      name
+      description
+
+      quantity
+      image
+      category {
         _id
       }
     }
   }
 `;
 
-export const ADD_ORDER = gql`
-  mutation addOrder($exercises: [ID]!) {
-    addOrder(exercises: $exercises) {
-      purchaseDate
-      exercises {
-        _id
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($exercises: [ID]!) {
+    checkout(exercises: $exercises) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_EXERCISE = gql`
+  {
+    exercises {
+      _id
+      name
+      description
+
+      quantity
+      sets
+      weight
+      category {
         name
-        description
-
-        quantity
-        sets
-        weight
-        category {
-          name
-        }
       }
     }
   }
 `;
 
-export const ADD_USER = gql`
-  mutation addUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-  ) {
-    addUser(
-      firstName: $firstName
-      lastName: $lastName
-      email: $email
-      password: $password
-    ) {
-      token
-      user {
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
         _id
+        purchaseDate
+        exercises {
+          _id
+          name
+          description
+          sets
+          weight
+          quantity
+          image
+        }
       }
     }
   }
